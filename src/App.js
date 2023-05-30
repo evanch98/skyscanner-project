d782ed4a9e83c9c6c2fabdded55ff643bb57ff1b
Kyaw Thu <evanch98@gmail.com>
2023-05-30 11:48:35 +0800

    add handleDateSelect and make App a class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BpkCode } from "@skyscanner/backpack-web/bpk-component-code";
 import BpkButton from "@skyscanner/backpack-web/bpk-component-button";
 import BpkText from "@skyscanner/backpack-web/bpk-component-text";
-import BpkCalendar from "@skyscanner/backpack-web/bpk-component-calendar";
+import BpkCalendar, {
+	CALENDAR_SELECTION_TYPE,
+} from "@skyscanner/backpack-web/bpk-component-calendar";
 import format from "date-fns/format";
 
 import { cssModules } from "@skyscanner/backpack-web/bpk-react-utils";
 
 import STYLES from "./App.scss";
+import component from "@skyscanner/backpack-web/bpk-component-text";
 
 const getClassName = cssModules(STYLES);
 
@@ -59,29 +62,51 @@ const daysOfWeek = [
 	},
 ];
 
-function App() {
-	return (
-		<div className={getClassName("App")}>
-			<header className={getClassName("App__header")}>
-				<div className={getClassName("App__header-inner")}>
-					<BpkText
-						tagName="h1"
-						textStyle="xxl"
-						className={getClassName("App__heading")}
-					>
-						Flight Schedule
+class App extends component {
+	constructor() {
+		super();
+
+		this.state = {
+			selectionConfiguration: {
+				type: CALENDAR_SELECTION_TYPE.single,
+				date: null,
+			},
+		};
+	}
+
+	handleDateSelect = (date) => {
+		this.setState({
+			selectionConfiguration: {
+				type: this.props.selectionConfiguration.type,
+				date: date,
+			},
+		});
+	};
+
+	render() {
+		return (
+			<div className={getClassName("App")}>
+				<header className={getClassName("App__header")}>
+					<div className={getClassName("App__header-inner")}>
+						<BpkText
+							tagName="h1"
+							textStyle="xxl"
+							className={getClassName("App__heading")}
+						>
+							Flight Schedule
+						</BpkText>
+					</div>
+				</header>
+				<main className={getClassName("App__main")}>
+					<BpkText tagName="p" className={getClassName("App__text")}>
+						To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to
+						reload.
 					</BpkText>
-				</div>
-			</header>
-			<main className={getClassName("App__main")}>
-				<BpkText tagName="p" className={getClassName("App__text")}>
-					To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to
-					reload.
-				</BpkText>
-				<BpkButton onClick={() => alert("It works!")}>Click me</BpkButton>
-			</main>
-		</div>
-	);
+					<BpkButton onClick={() => alert("It works!")}>Click me</BpkButton>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
